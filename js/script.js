83d0b8969369d0b92ed46a3732d41b9a2ec8be1b
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-20 18:31:24 +0100

    Implemented cycle for

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 // - per i multipli di 3 stampi “Fizz” al posto del numero
 // - per i multipli di 5 stampi “Buzz” al posto del numero
 // - per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log ('FizzBuzz');
+    } else if (i % 5 === 0) {
+        console.log ('Buzz');
+    } else if (i % 3 === 0) {
+        console.log ('Fizz');
+    } else {
+        console.log(i);
+    } 
+}
