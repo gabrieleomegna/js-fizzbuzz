3e87d4fb217137da0428c70e1165ce33a74d6611
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-20 18:56:17 +0100

    Created card modifying index.html and style.css, implemented for cycle in order to have cards in index

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@
 // - per i multipli di 3 stampi “Fizz” al posto del numero
 // - per i multipli di 5 stampi “Buzz” al posto del numero
 // - per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+const wrapperElement = document.querySelector ('div.wrapper');
+let cardElement;
+
 for (let i = 1; i <= 100; i++) {
+    cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    wrapperElement.appendChild (cardElement);
     if (i % 3 === 0 && i % 5 === 0) {
         console.log ('FizzBuzz');
     } else if (i % 5 === 0) {
